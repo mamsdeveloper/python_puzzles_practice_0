0e927a7cc9e30d325aaf1b3cc64cd2af9744bd4d
mamsdeveloper <belk1naalyona@yandex.ru>
2022-05-10 11:40:25 +0500

    12.py solved

---
+++ 12.py
@@ -0,0 +1,26 @@
+"""
+Question 13
+Level 2
+
+Question:
+Write a program that accepts a sentence and calculate the number of letters and digits.
+Suppose the following input is supplied to the program:
+hello world! 123
+Then, the output should be:
+LETTERS 10
+DIGITS 3
+
+"""
+
+
+s = input()
+d = {"DIGITS": 0, "LETTERS": 0}
+for c in s:
+    if c.isdigit():
+        d["DIGITS"] += 1
+    elif c.isalpha():
+        d["LETTERS"] += 1
+    else:
+        pass
+print("LETTERS", d["LETTERS"])
+print("DIGITS", d["DIGITS"])
