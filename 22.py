840f3f6c44e5cf6b3ece259edcace4d7065603e6
mamsdeveloper <belk1naalyona@yandex.ru>
2022-05-14 07:03:04 +0500

    22.py solved

---
+++ 22.py
@@ -0,0 +1,16 @@
+"""
+Question 23
+level 1
+
+Question:
+    Write a method which can calculate square value of number
+
+"""
+
+
+def square(num):
+    return num ** 2
+
+
+print(square(2))
+print(square(3))
