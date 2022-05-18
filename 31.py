dfd1556e1aabed873aba2df44ce541df03c59ed5
mamsdeveloper <belk1naalyona@yandex.ru>
2022-05-18 13:17:06 +0500

    31.py solved

---
+++ 31.py
@@ -0,0 +1,15 @@
+"""
+Question:
+Define a function that can accept an integer number as input and print(the "It is an even number" if the number is even, otherwise print("It is an odd number".)
+
+"""
+
+
+def checkValue(n):
+    if n % 2 == 0:
+        print("It is an even number")
+    else:
+        print("It is an odd number")
+
+
+checkValue(7)
